7f7ddabd0bcb518a2439ae82113b81714efc2f36
lachezar <lucho870601@gmail.com>
2023-12-09 11:19:51 +0100

    Day 9, but shorter

--- src/main/scala/se/yankov/aoc2023/Day9.scala
+++ src/main/scala/se/yankov/aoc2023/Day9.scala
@@ -7,29 +7,26 @@ import scala.annotation.tailrec
 
 object Day9 extends IOApp.Simple {
 
-  @tailrec
-  def findNext(sequence: Array[Int], aggregator: Int): Int =
-    if sequence.forall(_ == 0) then aggregator
-    else
-      val newSequence: Array[Int] = sequence.sliding(2).map((pair: Array[Int]) => pair(1) - pair(0)).toArray
-      findNext(newSequence, aggregator + sequence.last)
+  def findNext(sequence: Array[Int]): Int = findElement(sequence, (_.last), Nil).sum
+
+  def findPrev(sequence: Array[Int]): Int = findElement(sequence, (_.head), Nil).reduce((a: Int, b: Int) => b - a)
 
   @tailrec
-  def findPrev(sequence: Array[Int], headElements: List[Int]): List[Int] =
-    if sequence.forall(_ == 0) then headElements
+  def findElement(sequence: Array[Int], getter: (Array[Int] => Int), edgeElements: List[Int]): List[Int] =
+    if sequence.forall(_ == 0) then edgeElements
     else
       val newSequence: Array[Int] = sequence.sliding(2).map((pair: Array[Int]) => pair(1) - pair(0)).toArray
-      findPrev(newSequence, sequence.head :: headElements)
+      findElement(newSequence, getter, getter(sequence) :: edgeElements)
 
   val task1: IO[Unit] = for {
     lines: List[Array[Int]] <- Utils.readLines[IO]("day9.input.txt").map(_.split(' ').map(_.toInt)).compile.toList
-    res: Int                 = lines.map(findNext(_, 0)).sum
+    res: Int                 = lines.map(findNext).sum
     _                       <- IO.println(res)
   } yield ()
 
   val task2: IO[Unit] = for {
     lines: List[Array[Int]] <- Utils.readLines[IO]("day9.input.txt").map(_.split(' ').map(_.toInt)).compile.toList
-    res: Int                 = lines.map(findPrev(_, Nil).fold(0)((a: Int, b: Int) => b - a)).sum
+    res: Int                 = lines.map(findPrev).sum
     _                       <- IO.println(res)
   } yield ()
 
