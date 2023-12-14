212623a87bb15e64018d43cd3659bf9fdcf2e002
lachezar <lucho870601@gmail.com>
2023-12-14 18:39:44 +0100

    Day 14, but skipped normalization and shortened the code size

--- src/main/scala/se/yankov/aoc2023/Day14.scala
+++ src/main/scala/se/yankov/aoc2023/Day14.scala
@@ -2,59 +2,36 @@ package se.yankov.aoc2023
 
 import cats.effect.{ IO, IOApp }
 import scala.annotation.tailrec
+import util.chaining.scalaUtilChainingOps
 
 object Day14 extends IOApp.Simple {
 
-  final case class NormalizedRow(offset: Int, amount: Int):
-    def incrementAmount: NormalizedRow = copy(amount = amount + 1)
-
-  extension (field: Vector[Vector[Char]])
-    def render: String = field.map(_.mkString).mkString("\n")
-    def totalLoad: Int = field
-      .transpose
-      .map(_.reverse.zipWithIndex.collect { case 'O' -> i => i + 1 }.sum)
-      .sum
+  extension (field: Vector[String])
+    def render: String = field.mkString("\n")
+    def totalLoad: Int = field.transpose.map(_.reverse.zipWithIndex.collect { case 'O' -> i => i + 1 }.sum).sum
 
   def tilt(
-      field: Vector[Vector[Char]],
-      yTransformation: (Vector[Vector[Char]]) => Vector[Vector[Char]],
-      xTransformation: (Vector[Char]) => Vector[Char],
-    ): Vector[Vector[Char]] =
-    val normalizedField: Vector[Vector[NormalizedRow]] = yTransformation(field).map { (row: Vector[Char]) =>
-      xTransformation(row).zipWithIndex.foldLeft(Vector(NormalizedRow(0, 0))) {
-        case (acc: Vector[NormalizedRow], (c: Char, i: Int)) =>
-          c match
-            case '#' => acc :+ NormalizedRow(i + 1, 0)
-            case 'O' => acc.dropRight(1) :+ acc.last.incrementAmount
-            case _   => acc
-      }
-    }
-    val cleanLine: Vector[Char]                        = Vector.fill(field.length)('.')
-    yTransformation(normalizedField.map { (row: Vector[NormalizedRow]) =>
-      xTransformation(row.foldLeft(cleanLine) {
-        case (line: Vector[Char], NormalizedRow(offset, amount)) =>
-          // Restore the normalized matrix back to the original format
-          val lineWithSquareStone: Vector[Char] = if offset > 0 then line.updated(offset - 1, '#') else line
-          lineWithSquareStone.patch(offset, Vector.fill(amount)('O'), amount)
-      })
-    })
+      field: Vector[String],
+      yTransformation: (Vector[String]) => Vector[String],
+      xTransformation: (String) => String,
+    ): Vector[String] =
+    yTransformation(field).map(xTransformation(_).split("#", -1).map(_.sortBy(-_)).mkString("#")) pipe
+      ((transformedField: Vector[String]) => yTransformation(transformedField.map(xTransformation(_))))
 
-  def tiltNorth(field: Vector[Vector[Char]]): Vector[Vector[Char]] = tilt(field, _.transpose, identity)
+  def tiltNorth(field: Vector[String]): Vector[String] = tilt(field, _.transpose.map(_.mkString), identity)
 
-  def tiltWest(field: Vector[Vector[Char]]): Vector[Vector[Char]] = tilt(field, identity, identity)
+  def tiltWest(field: Vector[String]): Vector[String] = tilt(field, identity, identity)
 
-  def tiltSouth(field: Vector[Vector[Char]]): Vector[Vector[Char]] = tilt(field, _.transpose, _.reverse)
+  def tiltSouth(field: Vector[String]): Vector[String] = tilt(field, _.transpose.map(_.mkString), _.reverse)
 
-  def tiltEast(field: Vector[Vector[Char]]): Vector[Vector[Char]] = tilt(field, identity, _.reverse)
+  def tiltEast(field: Vector[String]): Vector[String] = tilt(field, identity, _.reverse)
 
-  val cycle: Vector[Vector[Char]] => Vector[Vector[Char]] =
-    tiltEast compose tiltSouth compose tiltWest compose tiltNorth
+  val cycle: Vector[String] => Vector[String] = tiltEast compose tiltSouth compose tiltWest compose tiltNorth
 
   @tailrec
-  def computeBillionthCycle(field: Vector[Vector[Char]], previousCycles: Vector[Vector[Vector[Char]]])
-      : Vector[Vector[Char]] =
-    val res: Vector[Vector[Char]] = cycle(field)
-    val matchingCycleIndex: Int   = previousCycles.indexOf(res)
+  def computeBillionthCycle(field: Vector[String], previousCycles: Vector[Vector[String]]): Vector[String] =
+    val res: Vector[String]     = cycle(field)
+    val matchingCycleIndex: Int = previousCycles.indexOf(res)
     if matchingCycleIndex > -1 then {
       val cyclePeriod: Int         = previousCycles.length - matchingCycleIndex
       val billionthCycleIndex: Int = (1_000_000_000 - matchingCycleIndex) % cyclePeriod + matchingCycleIndex - 1
@@ -63,15 +40,15 @@ object Day14 extends IOApp.Simple {
     else computeBillionthCycle(res, previousCycles :+ res)
 
   val task1: IO[Unit] = for {
-    field: Vector[Vector[Char]] <- Utils.readLines[IO]("day14.input.txt").map(_.toVector).compile.toVector
-    res: Int                     = tiltNorth(field).totalLoad
-    _                           <- IO.println(res)
+    field: Vector[String] <- Utils.readLines[IO]("day14.input.txt").compile.toVector
+    res: Int               = tiltNorth(field).totalLoad
+    _                     <- IO.println(res)
   } yield ()
 
   val task2: IO[Unit] = for {
-    field: Vector[Vector[Char]] <- Utils.readLines[IO]("day14.input.txt").map(_.toVector).compile.toVector
-    res: Int                     = computeBillionthCycle(field, Vector.empty).totalLoad
-    _                           <- IO.println(res)
+    field: Vector[String] <- Utils.readLines[IO]("day14.input.txt").compile.toVector
+    res: Int               = computeBillionthCycle(field, Vector.empty).totalLoad
+    _                     <- IO.println(res)
   } yield ()
 
   def run: IO[Unit] = task1 >> task2
