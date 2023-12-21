368f108b8efee827b47b6aa57e56f9cf7e5c27ea
lachezar <lucho870601@gmail.com>
2023-12-21 15:59:37 +0100

    Changed Day solution to work with Long (so that Day 21 can reuse it)

--- src/main/scala/se/yankov/aoc2023/Day9.scala
+++ src/main/scala/se/yankov/aoc2023/Day9.scala
@@ -5,28 +5,28 @@ import scala.annotation.tailrec
 
 object Day9 extends IOApp.Simple {
 
-  def findNext(sequence: Array[Int]): Int = findElement(sequence, (_.last), Nil).sum
+  def findNext(sequence: Array[Long]): Long = findElement(sequence, (_.last), Nil).sum
 
-  def findPrev(sequence: Array[Int]): Int = findElement(sequence, (_.head), Nil).reduce((a: Int, b: Int) => b - a)
+  def findPrev(sequence: Array[Long]): Long = findElement(sequence, (_.head), Nil).reduce((a: Long, b: Long) => b - a)
 
   @tailrec
-  def findElement(sequence: Array[Int], getter: (Array[Int] => Int), edgeElements: List[Int]): List[Int] =
+  def findElement(sequence: Array[Long], getter: (Array[Long] => Long), edgeElements: List[Long]): List[Long] =
     if sequence.forall(_ == 0) then edgeElements
     else
-      val newSequence: Array[Int] = sequence.sliding(2).map((pair: Array[Int]) => pair(1) - pair(0)).toArray
+      val newSequence: Array[Long] = sequence.sliding(2).map((pair: Array[Long]) => pair(1) - pair(0)).toArray
       findElement(newSequence, getter, getter(sequence) :: edgeElements)
 
   val task1: IO[Unit] = for {
-    lines: List[Array[Int]] <- Utils.readLines[IO]("day9.input.txt").map(_.split(' ').map(_.toInt)).compile.toList
-    res: Int                 = lines.map(findNext).sum
-    _                       <- IO.println(res)
+    lines: List[Array[Long]] <- Utils.readLines[IO]("day9.input.txt").map(_.split(' ').map(_.toLong)).compile.toList
+    res: Long                 = lines.map(findNext).sum
+    _                        <- IO.println(res)
   } yield ()
 
   val task2: IO[Unit] = for {
-    lines: List[Array[Int]] <- Utils.readLines[IO]("day9.input.txt").map(_.split(' ').map(_.toInt)).compile.toList
-    res: Int                 = lines.map(findPrev).sum
-    _                       <- IO.println(res)
+    lines: List[Array[Long]] <- Utils.readLines[IO]("day9.input.txt").map(_.split(' ').map(_.toLong)).compile.toList
+    res: Long                 = lines.map(findPrev).sum
+    _                        <- IO.println(res)
   } yield ()
 
-  def run: IO[Unit] = task2
+  def run: IO[Unit] = task1 >> task2
 }
